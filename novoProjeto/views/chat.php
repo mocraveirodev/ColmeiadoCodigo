7a5ffeb8bd7e955e3e4c267446b53a6bdf54f9bd
Danilo Teixeira <danilo.tx13@gmail.com>
2019-11-13 09:03:44 -0300

    atualizando a pagina do chat

---
+++ novoProjeto/views/chat.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chat</title>
+    <link rel="shortcut icon" href="views/img/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel=stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="chat col-10">
+                <div class="dtTeste">
+                    <div class="chatHeader">
+                        <div class="menuHamburguer">
+                            <img src="img/menuHamburguer.svg" alt="">
+                        </div>
+                        <div class="chatTitle">
+                            <h1>Chat</h1>
+                        </div>
+                        <div class="fotoUsuario">
+                            <img src="img/profileImg.svg" alt="foto do usuario">
+                        </div>
+                    </div>
+
+                    <div class="mensagemRecebida">
+                        <div class="perfilChat">
+                            <img src="img/profileMarcelo.svg" alt="foto do usuario">
+                            <p>Marcelo Bras</p>
+                        </div>
+                        <div class="conteudoMensagem">
+                            <p>Alguém pode me ajudar?</p>
+                        </div>
+                    </div>
+
+                    <div class="mensagemRecebida">
+                        <div class="perfilChat">
+                            <img src="img/profileLuana.svg" alt="foto do usuario">
+                            <p>Luana Silva</p>
+                        </div>
+                        <div class="conteudoMensagem">
+                            <p>Claro, o que foi?</p>
+                        </div>
+                    </div>
+
+                    <div class="mensagemEnviada">
+                        <div class="conteudoMensagemSend">
+                            <p>Oi Galera, tudo bem?</p>
+                        </div>
+                        <div class="meuPerfilChat">
+                            <img src="img/profileImg.svg" alt="foto do usuario">
+                            <p>Júlia Mel</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="dtTesteDois">
+                    <div class="campoMensagem">
+                        <div class="digitarMensagem">
+                            <p>digite sua mensagem</p>
+                        </div>
+                        <div class="botaoDeEnvio">
+                            <img src="img/iconSend.png" alt="botao de envio">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bpOnline col-2">
+                <h2>Participantes (25)</h2>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+                <p>Mariana Luz</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
