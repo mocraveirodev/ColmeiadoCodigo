72b63daf328631ff2c4effaeefd73f92018328b5
Anaê Soares <anae08@gmail.com>
2019-12-06 12:09:37 -0200

    criando template.blade direito

--- resources/views/layouts/templateColmeia.blade.php
+++ resources/views/layouts/templateColmeia.blade.php
@@ -30,133 +30,46 @@
                 </div>
             </div>
         </section>
-        <!-- area do aluno -->
-        <section class="conteudo flex-grow-1 d-none">
-            <h1 class="titulo">Colmeia</h1>
-            <div class="cursos">
-                <a href="#" class="btnCurso html">HTML5</a>
-                <a href="#" class="btnCurso css"><img src="views\img\cadeado.svg" alt="Cadeado"> CSS</a>
-                <a href="#" class="btnCurso js"><img src="views\img\cadeado.svg" alt="Cadeado"> JAVASCRIPT</a>
-            </div>
-            <div class="progresso">
-                <p>Meta Diária</p>
-                <div class="progress">
-                    <div class="progress-bar bg-warning" role="progressbar" style="width: 50%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="progressoPorc">50%</p>
-            </div>
-        </section>        
         
-        <!-- Chat -->
-        <section class="conteudo flex-grow-1">
-            <h1 class="titulo">Chat</h1>
-            <div class="salaChat">
-                <div class="chat d-flex flex-row">
-                    <div class="message-group col-md-3 d-block">
-                        <div class="row justify-content-center">                
-                            <img src="views/img/profileMarcelo.svg" class="profile" alt="Imagem do Marcelo">
+        <main class="container">
+            <!-- Conteúdo do site -->
+            <div class="row">
+               <section class="col-md-2">
+                    <!-- Menu lateral esquerdo aqui vai o include -->
+                    <section class="menuResp">
+                        <a href="/ColmeiadoCodigo/"><img class="btnResp" src="views\img\inicio.svg" alt="Home"></a>
+                        <a href="#"><img class="btnResp" src="views\img\chat.svg" alt="Chat"></a>
+                        <a href="#"><img class="btnResp ativoResp" src="views\img\areaDoAluno.svg" alt="Inicio"></a>
+                        <a href="#"><img class="btnResp" src="views\img\conquistas.svg" alt="Conquistas"></a>
+                        <img src="views/img/profileImg.svg" class="profileResp" alt="Imagem de Perfil" onclick="menuAluno()">
+                    </section>
+               </section>
+               <section class="col-md-8">
+                <!-- Conteúdo aqui vai o include-->
+                @yield ('conteudo')
+               </section>
+               <section class="col-md-2">
+                    <!-- Menu lateral direito aqui vai o include-->
+                    <section class="menuAluno">
+                        <img src="views/img/profileImg.svg" class="profile" alt="Imagem de Perfil" onclick="menuAluno()">
+                        <div class="menuDireito">
+                            <div class="botoes">
+                                <img src="views/img/profileImg.svg" class="profile" alt="Imagem de Perfil" onclick="menuAluno()">
+                                <div class="navegacao">
+                                    <a href="#" ><img class="btnAluno" src="views\img\notificacaoAtivo.svg" alt="Notificação"></a>
+                                    <a href="#" ><img class="btnAluno" src="views\img\chatNotificacao.svg" alt="Chat"></a>
+                                    <a href="#" ><img class="btnAluno" src="views\img\configuracoes.svg" alt="Configurações"></a>
+                                    <a href="#" ><img class="btnAluno" src="views\img\ajuda.svg" alt="Ajuda"></a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="row justify-content-center">
-                            <span class="message-person">Marcelo Bras</span>
-                        </div> 
-                    </div>
-                    <div class="message-chat col-md-6">
-                        <div class="row justify-content">
-                            <p class="message-colmeia">Alguém pode me ajudar?</p>
-                        </div>                 
-                    </div>
-                </div>
-                <div class="chat d-flex flex-row">
-                    <div class="message-group col-md-3 d-block">
-                        <div class="row justify-content-center">                
-                            <img src="views/img/profileLuana.svg" class="profile" alt="Imagem da Luana">
-                        </div>
-                        <div class="row justify-content-center">
-                            <span class="message-person">Luana Silva</span>
-                        </div> 
-                    </div>
-                    <div class="message-chat col-md-6">
-                        <div class="row justify-content">
-                            <p class="message-colmeia">Claro, o que foi?</p>
-                        </div>                 
-                    </div>
-                </div>                
-                <div class="chat d-flex flex-row-reverse">
-                    <div class="message-group col-md-3 d-block">
-                        <div class="row justify-content-center">  
-                            <img src="views/img/profileImg.svg" class="profile" alt="Imagem de Perfil">
-                        </div>
-                        <div class="row justify-content-center">
-                            <span class="message-profile">Julia Mel</span>
-                        </div>
-                    </div> 
-                    <div class="message-chat col-md-6">
-                        <div class="row justify-content-end">
-                            <p class="message-logged">Oi galera, tudo bem?</p>
-                        </div>                 
-                    </div>
-                </div>
-                
-            </div>
-            <div class="input-group m-15">
-                <textarea class="form-control custom-control" placeholder="Digite sua mensagem..." rows="3" style="resize:none"></textarea>     
-                <span class="input-group-addon btn btn-warning"><i class="fa fa-paper-plane fa-lg" aria-hidden="true"></i></span>
-            </div>                    
-        </section>
-        <section>
-            <div class="participantes flex-md-column">
-                <h5>Participantes (25)</h5>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-                <p>Mariana Luz</p>
-            </div>
-        </section>
+                    </section>             
+                </section>
 
-        <section class="menuAluno">
-            <img src="views/img/profileImg.svg" class="profile" alt="Imagem de Perfil" onclick="menuAluno()">
-            <div class="menuDireito">
-                <div class="botoes">
-                    <img src="views/img/profileImg.svg" class="profile" alt="Imagem de Perfil" onclick="menuAluno()">
-                    <div class="navegacao">
-                        <a href="#" ><img class="btnAluno" src="views\img\notificacaoAtivo.svg" alt="Notificação"></a>
-                        <a href="#" ><img class="btnAluno" src="views\img\chatNotificacao.svg" alt="Chat"></a>
-                        <a href="#" ><img class="btnAluno" src="views\img\configuracoes.svg" alt="Configurações"></a>
-                        <a href="#" ><img class="btnAluno" src="views\img\ajuda.svg" alt="Ajuda"></a>
-                    </div>
-                </div>
             </div>
-        </section>       
+        </main>
 
-        <section class="menuResp">
-            <a href="/ColmeiadoCodigo/"><img class="btnResp" src="views\img\inicio.svg" alt="Home"></a>
-            <a href="#"><img class="btnResp" src="views\img\chat.svg" alt="Chat"></a>
-            <a href="#"><img class="btnResp ativoResp" src="views\img\areaDoAluno.svg" alt="Inicio"></a>
-            <a href="#"><img class="btnResp" src="views\img\conquistas.svg" alt="Conquistas"></a>
-            <img src="views/img/profileImg.svg" class="profileResp" alt="Imagem de Perfil" onclick="menuAluno()">
-        </section>
+        
     </main>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
