314aded539eb5aa042500d8e464a5a281a2004ce
Monica Craveiro <craveiromonica@gmail.com>
2020-01-18 10:50:55 -0300

    Excluindo arquivos nao usados

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,8 +25,18 @@
     @endif -->
     @include('includes.header')
 
-    @include('includes.menuDir')
-
+    <div class="menuDireito">
+        <div class="botoes">
+            <img src="img/profileImg.svg" class="profile mr-3" alt="Imagem de Perfil" onclick="menuAluno()">
+            <div class="navegacao">
+                <a href="/notificacao" ><img class="btnAluno" src="img/notificacaoAtivo.svg" alt="Notificação"></a>
+                <a href="/chat" ><img class="btnAluno" src="img/chatNotificacao.svg" alt="Chat"></a>
+                <a href="/config" ><img class="btnAluno" src="img/configuracoes.svg" alt="Configurações"></a>
+                <a href="/ajuda" ><img class="btnAluno" src="img/ajuda.svg" alt="Ajuda"></a>
+                <form method="post" action="{{ route('logout') }}" > @csrf <button type="submit" class="btn text-warning">logout</button></form>
+            </div>
+        </div>
+    </div>
     <main id="home">
         <section id="banner" >
             <div class="container">
